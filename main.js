a890a31d43e2502fb44671af456edd9939dd1211
Woody Maddox <woody.e.maddox@gmail.com>
2019-03-04 19:56:01 -0600

    if else

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("hi");
+const printToDom = (divId, textToPrint) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+
+
+const iAmThursty = (num) => {
+    //do some stuff
+    // under 21 'drink some water'
+    // over 21 but under 65 'have a beer'
+    // over 65 'take a nap'
+
+
+
+if ( num< 21){
+    return 'drink some water';
+
+}
+    else if (num > 21 && num <65){
+     
+        return "have a beer"}
+
+        else{
+            return "take a nap" 
+        
+}
+    
+
+};
+
+printToDom('thirsty', iAmThursty(55));
+
+
+console.log(iAmThursty(55));
+console.log(iAmThursty(11));
+console.log(iAmThursty(67));
+
+
+
+//Bonus = printToDOm function
